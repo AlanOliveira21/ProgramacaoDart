f5b3a57e29a2e6d218fc1990511832e362ad1444
Alan Oliveira <alan.oliveira298158@gmail.com>
2026-09-04 20:20:45 -0300

    Create 17-list.dart

---
+++ 17-list.dart
@@ -0,0 +1,10 @@
+void main() {
+  
+  List<String> alunos = ['Adryan', 'Alan', 'Ariel', 'João'];
+  print(alunos);
+  alunos.add('Bruna');
+  print(alunos);
+  alunos.remove('João');
+  print(alunos);
+  print(alunos[0]);
+}
